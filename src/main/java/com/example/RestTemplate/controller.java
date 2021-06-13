9aa7ce386228bcc4e86cd07002e68eac2e1c41d1
karthik-kore <kore8728@gmail.com>
2021-06-13 11:08:12 +0530

    Initial commit

---
+++ src/main/java/com/example/RestTemplate/controller.java
@@ -0,0 +1,31 @@
+package com.example.RestTemplate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class controller {
+    final String ri="http://localhost:8080/student";
+    @Autowired
+    RestTemplate restTemplate;
+    @RequestMapping(value = "/get")
+    public String restTemplateExample() {
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> httpEntity = new HttpEntity<String>(headers);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(ri, HttpMethod.GET, httpEntity, String.class);
+        return responseEntity.getBody();
+
+    }
+    @RequestMapping(value = "/get",method = RequestMethod.POST)
+    public String postRest(@RequestBody student k){
+        HttpHeaders headers=new HttpHeaders();
+        HttpEntity<Object> entity=new HttpEntity<Object>(k,headers);
+        return restTemplate.exchange(ri,HttpMethod.POST,entity,String.class).getBody();
+
+    }
+}
---
+++ src/main/java/com/example/RestTemplate/student.java
@@ -0,0 +1,36 @@
+package com.example.RestTemplate;
+
+public class student {
+    int id;
+    String name;
+
+
+    public student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
